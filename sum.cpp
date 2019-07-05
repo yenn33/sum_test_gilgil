eabec80c680520b55561018231ef02bb2e666c89
yenn33 <yenn_33@naver.com>
2019-07-05 01:43:16 -0700

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,14 @@
 #include "sum.h"
 
 int sum(int n){
-	int r = 0;
-	for(int i = 1; i <= n; i++) r += i;
-	return r;
-
+	return n*(n+1)/2;
+//code upgrade
 }
+
+//int sum(int n){
+//	int r = 0;
+//	for(int i = 1; i <= n; i++) r += i;
+//	return r;
+
+//}
 
